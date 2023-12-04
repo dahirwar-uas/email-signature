676363169df019312996fd66299a6bef4abecb94
dahirwar-uas <108655888+dahirwar-uas@users.noreply.github.com>
2023-12-04 19:14:03 +0530

    update html template

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -21,7 +21,7 @@ export class EmployeeComponent implements OnInit {
   // response:string;
   
   signature: any = {
-    "region": '', "team": '', "employeeName": '', "employeeDesignation": '', "addressLine1": '', "addressLine2": '', "addressLine3": '', "phone": '', "faxLine": '', "mobile": '', "extLine": ''
+    "region": '', "team": '', "teamName": '', "employeeName": '', "employeeDesignation": '', "addressLine1": '', "addressLine2": '', "addressLine3": '', "phone": '', "faxLine": '', "mobile": '', "extLine": ''
   };
 
   regions = ['Middle East Headquarters', 'India'];
@@ -128,9 +128,17 @@ export class EmployeeComponent implements OnInit {
   }
   validityState() {
     if(this.signature) {
+      if (this.signature.team == 'Department') {
+        this.signature.employeeName = 'Dummy'; 
+        this.signature.employeeDesignation = 'Dummy';
+      }
+      if (this.signature.team == 'Business') {
+        this.signature.teamName = 'Dummy';
+      }
       this.error = false;
       const keys = Object.keys(this.signature);
       keys.forEach( (e: any) => {
+        console.log("this.signature[e]: ", this.signature[e])
         if(this.signature[e] == "") {
           this.error = true;
         }
